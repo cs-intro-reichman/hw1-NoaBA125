0198d4b2cd8ab3792248dc801d8179bf10ea3a84
NoaBA125 <noa.benami10@gmail.com>
2024-11-11 17:36:21 +0200

    first update

--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,14 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+	    String name1 = (args[0]);
+	    String name2 = (args[1]);
+		String name3 = (args[2]);
+
+		int i = Integer.parseInt(args[3]);
+		double result = (i/3.0);
+		double rounded = Math.ceil(result);
+
+		System.out.println("pay " + rounded + " shekels each.");   
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,13 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int number = Integer.parseInt(args[0]);
+
+		int h = (number/100);
+		int t = ((number/10)%10);
+		int o = number%10;
+
+
+		System.out.println(h + " hundreds, " + t + " tens, " + o + " ones.");
 	}
 }
